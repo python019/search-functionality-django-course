62dea15abc39a323797b5614d360725b2504f6bc
python019 <tommyjumayev@gmail.com>
2022-04-28 09:29:41 +0500

    7-dars

--- searchdjango/settings.py
+++ searchdjango/settings.py
@@ -106,6 +106,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 

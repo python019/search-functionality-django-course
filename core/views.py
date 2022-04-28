b1d22a0b64f76ef80c941d51ce98f457fcec4e22
python019 <tommyjumayev@gmail.com>
2022-04-28 08:54:47 +0500

    6-dars

--- core/views.py
+++ core/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .models import Ishchilar
+from django.db.models import Q
 
 def home(request):
     if 'q' in request.GET:
         qidirish_sozi = request.GET['q']
-        ishchilar = Ishchilar.objects.filter(ismi__icontains=qidirish_sozi)
+        toliq = Q(Q(ismi__icontains=qidirish_sozi) | Q(familiyasi__icontains=qidirish_sozi) | Q(yoshi__icontains=qidirish_sozi) | Q(kasbi__icontains=qidirish_sozi))
+        ishchilar = Ishchilar.objects.filter(toliq)
     else:
         ishchilar = Ishchilar.objects.all()
 
